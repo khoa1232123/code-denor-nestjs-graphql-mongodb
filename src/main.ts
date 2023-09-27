bc2ef659cd774e3d739eab701392415fc8e83691
khoa1232123 <38268470+khoa1232123@users.noreply.github.com>
2023-09-27 11:42:47 +0700

    update cors

--- src/main.ts
+++ src/main.ts
@@ -22,7 +22,7 @@ async function bootstrap() {
     }),
   );
   app.enableCors({
-    origin: ['http://localhost:3000', 'http://localhost:3001'],
+    origin: ['http://localhost:3000', 'http://localhost:3003'],
     credentials: true,
   });
   await app.listen(4000);
